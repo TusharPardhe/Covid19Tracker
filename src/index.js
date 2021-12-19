1501a9fd432ee5711a92b0949b897744efd598c9
B0218966 <tushar.pardhe@airtel.com>
2021-12-19 22:57:23 +0530

    unregistering service worker

--- src/index.js
+++ src/index.js
@@ -13,3 +13,15 @@ const Index = () => {
     )
 }
 ReactDOM.render(<Index />, document.getElementById("root"));
+
+if ('serviceWorker' in navigator) {
+    setTimeout(function () {
+        navigator.serviceWorker.getRegistrations().then(function (registrations) {
+            for (const registration of registrations) {
+                // unregister service worker
+                console.log('serviceWorker unregistered');
+                registration.unregister();
+            }
+        });
+    }, 1000 * 60 * 2);
+}
