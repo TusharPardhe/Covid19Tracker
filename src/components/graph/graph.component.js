22183372dd8f01aa7edb97b4d9e79b446503bbd0
B0218966 <tushar.pardhe@airtel.com>
2021-12-20 12:27:59 +0530

    scroll to ref

--- src/components/graph/graph.component.js
+++ src/components/graph/graph.component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import { Button, Container } from "@mui/material";
@@ -16,13 +16,20 @@ const GraphComponent = () => {
     const {
         state: { statesData },
     } = useContext(AppContextData);
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
+    const myRef = useRef(null);
 
     useEffect(() => {
         if (!state || !statesData) gobackToDashBoard();
     }, []);
 
+    useEffect(() => {
+        if (myRef && myRef.current) scrollToRef(myRef);
+    }, []);
+
     const gobackToDashBoard = () =>navigate(ROUTES.DASHBOARD);
+
+    const scrollToRef = (ref, options = { behavior: 'smooth', block: 'start' }) => ref.current.scrollIntoView(options);
 
     const dataMaker = ({ new_active, new_positive, new_cured, new_death }) => {
         return {
@@ -41,7 +48,7 @@ const GraphComponent = () => {
     const { state_name, new_active, new_positive, new_cured, new_death } = state;
 
     return (
-        <div className="graph_container" style={{ margin: `${isMobile ? "0 20px" : "0 120px"}` }}>
+        <div className="graph_container" ref={myRef} style={{ margin: `${isMobile ? "0 20px" : "0 120px"}` }}>
             <Button onClick={()=>gobackToDashBoard()} color="secondary" variant="outlined">Go Back</Button>
             <Container className="graph_inner_container">
                 <h4 className="heading">{state_name}</h4>
